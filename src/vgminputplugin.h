f485d09e3e95115d7120a9d37ee3552dbd78fda2
Luke Taylor <luket@pm.me>
2026-09-25 23:40:57 +0100

    Use shared plugin IID

--- src/vgminputplugin.h
+++ src/vgminputplugin.h
@@ -30,7 +30,7 @@ class VgmInputPlugin : public QObject,
                        public PluginConfigGuiPlugin
 {
     Q_OBJECT
-    Q_PLUGIN_METADATA(IID "org.fooyin.fooyin.plugin" FILE "vgminput.json")
+    Q_PLUGIN_METADATA(IID FOOYIN_PLUGIN_IID FILE "vgminput.json")
     Q_INTERFACES(Fooyin::Plugin Fooyin::InputPlugin Fooyin::PluginConfigGuiPlugin)
 
 public:
